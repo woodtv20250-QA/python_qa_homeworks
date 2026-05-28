7abe3c639b1240e2e4c6f7a7945b715d184b0115
Татьяна <woodtv2014@gmail.com>
2026-05-28 21:32:15 +0700

    feat: add lesson6K homework

---
+++ 06K_lesson/test_02_calc.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install())
+)
+
+driver.get(
+    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+)
+
+delay_field = driver.find_element(By.CSS_SELECTOR, '#delay')
+delay_field.clear()
+delay_field.send_keys("45")
+
+driver.find_element(By.XPATH, "//span[text()='7']").click()
+driver.find_element(By.XPATH, "//span[text()='+']").click()
+driver.find_element(By.XPATH, "//span[text()='8']").click()
+driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+wait = WebDriverWait(driver, 50)
+wait.until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+)
+
+result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+assert result == "15", f"Ожидалось 15, получено {result}"
+
+print("Тест пройден: 7 + 8 = 15")
+
+driver.quit()
